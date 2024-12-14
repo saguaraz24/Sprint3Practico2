63bd0733c0e16bda4c944cafe01e1125660493e1
Sebastian Guaraz <sebastianadresguaraz@gmail.com>
2024-12-14 20:00:18 -0300

    Update superheroe.mjs

--- src/models/superheroe.mjs
+++ src/models/superheroe.mjs
@@ -17,16 +17,16 @@ const superheroeSchema = new mongoose.Schema({
       minlength: [3, 'El nombre real debe tener al menos 3 caracteres'], 
       maxlength: [60, 'El nombre real no puede tener más de 60 caracteres']
     },  
- edad: { type: Number}, 
-       //required//: [true, 'La edad es requerida'], 
-    //   trim: true, min: [0, 'La edad no puede ser negativa'], 
-    //   validate: { validator: Number.isInteger, 
-    //   message: 'La edad debe ser un número entero' }
-    // }, 
+    edad: { type: Number, 
+      required: [true, 'La edad es requerida'], 
+      trim: true, min: [0, 'La edad no puede ser negativa'], 
+      validate: { validator: Number.isInteger, 
+      message: 'La edad debe ser un número entero' }
+    }, 
 
-     planetaOrigen: { type: String},//, default: 'Desconocido' },  
-     debilidad: String,  
-     poderes: {type: [String]},//, required: [true, 'Los poderes son requeridos'], validate: { validator: function(array) { return array.length > 0 && array.every(poder => poder.trim().length >= 3 && poder.trim().length <= 60); }, message: 'Cada poder debe tener entre 3 y 60 caracteres sin espacios en blanco' }},  
+    planetaOrigen: { type: String, default: 'Desconocido' },  
+    debilidad: String,  
+    poderes: {type: [String], required: [true, 'Los poderes son requeridos'], validate: { validator: function(array) { return array.length > 0 && array.every(poder => poder.trim().length >= 3 && poder.trim().length <= 60); }, message: 'Cada poder debe tener entre 3 y 60 caracteres sin espacios en blanco' }},  
     aliados: [String],  
     enemigos: [String],  
     createdAt: { type: Date, default: Date.now }  
